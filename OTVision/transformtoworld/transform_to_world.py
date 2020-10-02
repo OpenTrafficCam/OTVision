bce3b3567d7715f7fd67582483b66742d9ab21df
martinbaerwolff <maritn.baerwolff@tu-dresden.de>
2020-10-02 19:35:25 +0200

    Fixed PEP-8 line length violations

--- OTVision/transformtoworld/transform_to_world.py
+++ OTVision/transformtoworld/transform_to_world.py
@@ -19,20 +19,30 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
-This module calculates a homography matrix and transforms trajectory points from pixel into world coordinates.
-It requires reference points in both pixel and world coordinates as well as trajectories in pixel coordinates.
-Reference points have to be loaded from .npy files (or from semicolon delimited .txt files without headers or indices).
-Multiple trajectory files with data arranged in otc style (OpenTrafficCam) can be selected,  preferably from .pkl files.
+This module calculates a homography matrix and transforms trajectory points from pixel
+into world coordinates.
+It requires reference points in both pixel and world coordinates as well as
+trajectories in pixel coordinates.
+Reference points have to be loaded from .npy files (or from semicolon delimited .txt
+files without headers or indices).
+Multiple trajectory files with data arranged in otc style (OpenTrafficCam) can be
+selected,  preferably from .pkl files.
 The module saves the trajectories in world coordinates as .pkl files.
-For different camera views, the module has to be run repeatedly with respective reference points in pixel coordinates.
+For different camera views, the module has to be run repeatedly with respective
+reference points in pixel coordinates.
 
 Additional information due to OpenCV shortcoming:
-Some extra calculations are performed, because OpenCV functions cannot deal with all digits of UTM coordinates.
-Hence, from "refpts_world" only the three digits before and all digits after delimiter are used for transformation.
+Some extra calculations are performed, because OpenCV functions cannot deal with all
+digits of UTM coordinates.
+Hence, from "refpts_world" only the three digits before and all digits after delimiter
+are used for transformation.
 After transformation the truncated thousands digits are concatenated again.
-To avoid errors due to different thousands digits within study area, before transformation "refpts_world" are shifted,
-so that the center of all reference points lies on a round 500 m value for both x & y UTM coordinates.
-After transformation the "traj_world" coordinates are shifted back by exactly the same value.
+To avoid errors due to different thousands digits within study area, before
+transformation "refpts_world" are shifted,
+so that the center of all reference points lies on a round 500 m value for both x & y
+UTM coordinates.
+After transformation the "traj_world" coordinates are shifted back by exactly the same
+value.
 
 To Dos:
 - Save also homography as npy file?
@@ -49,18 +59,22 @@ TEST_DATA_FOLDER = r"..\\..\\tests\\data"
 
 
 def read_refpts_pixel_dialog(default_folder=TEST_DATA_FOLDER):
-    """User can select one file containing reference points in pixel coordinates in npy or csv format
-    (generated with getRefPts.py) and they are read to a numpy array.
+    """User can select one file containing reference points in pixel coordinates in npy
+    or csv format (generated with getRefPts.py) and they are read to a numpy array.
 
     Keyword arguments:
     default_folder -- Default path when opening the file browser
     """
     try:
-        refpts_pixel_path = filedialog.askopenfilename(initialdir=default_folder,
-                                              title="Select reference points in pixel coordinates (.txt or .npy)",
-                                              filetypes=(("Numpy files", "*.npy"),
-                                                         ("Text files", "*.txt"),
-                                                         ("all files", "*.*")))
+        refpts_pixel_path = filedialog.askopenfilename(
+            initialdir=default_folder,
+            title="Select reference points in pixel coordinates (.txt or .npy)",
+            filetypes=(
+                ("Numpy files", "*.npy"),
+                ("Text files", "*.txt"),
+                ("all files", "*.*"),
+            ),
+        )
         if refpts_pixel_path.endswith(".npy"):
             print(refpts_pixel_path + " is a numpy file")
             refpts_pixel = np.load(refpts_pixel_path)
@@ -74,19 +88,23 @@ def read_refpts_pixel_dialog(default_folder=TEST_DATA_FOLDER):
 
 
 def read_refpts_world_dialog(default_folder=TEST_DATA_FOLDER):
-    """User can select one file containing reference points in world coordinates in npy or csv format
-    (generated with getRefPts.py) and they are read to a numpy array.
+    """User can select one file containing reference points in world coordinates in npy
+    or csv format (generated with getRefPts.py) and they are read to a numpy array.
 
     Keyword arguments:
     default_folder -- Default path when opening the file browser
     """
 
     try:
-        refpts_world_path = filedialog.askopenfilename(initialdir=default_folder,
-                                              title="Select reference points in World coordinates (.txt or .npy)",
-                                              filetypes=(("Numpy files", "*.npy"),
-                                                         ("Text files", "*.txt"),
-                                                         ("all files", "*.*")))
+        refpts_world_path = filedialog.askopenfilename(
+            initialdir=default_folder,
+            title="Select reference points in World coordinates (.txt or .npy)",
+            filetypes=(
+                ("Numpy files", "*.npy"),
+                ("Text files", "*.txt"),
+                ("all files", "*.*"),
+            ),
+        )
         if refpts_world_path.endswith(".npy"):
             print(refpts_world_path + " is a numpy file")
             refpts_world = np.load(refpts_world_path)
@@ -99,18 +117,22 @@ def read_refpts_world_dialog(default_folder=TEST_DATA_FOLDER):
     return refpts_world
 
 
-def choose_traj_pixel_dialog(default_folder=TRAJ_PATH_TEST):
+def choose_traj_pixel_dialog(default_folder=TEST_DATA_FOLDER):
     """User can select one or multiple trajectory files in pkl or csv format.
 
     Keyword arguments:
     default_folder -- Default path when opening the file browser
     """
     try:
-        traj_pixel_paths = filedialog.askopenfilenames(initialdir=default_folder,
-                                              title="Select converted DataFromSky trajectories in pixel coordinates (.npy)",
-                                              filetypes=(("Python pickle files", "*.pkl"),
-                                                         ("CSV files", "*.csv"),
-                                                         ("All files", "*.*")))
+        traj_pixel_paths = filedialog.askopenfilenames(
+            initialdir=default_folder,
+            title="Select DataFromSky trajectories in pixel coordinates (.npy)",
+            filetypes=(
+                ("Python pickle files", "*.pkl"),
+                ("CSV files", "*.csv"),
+                ("All files", "*.*"),
+            ),
+        )
     except:
         print("Please try again")
         return read_traj_pixel_dialog(default_folder)
@@ -138,14 +160,16 @@ def read_traj_pixel_dialog(traj_pixel_path):
 
 
 def calculate_homography_matrix(refpts_pixel, refpts_world):
-    """Calculate homography matrix using pixel and world coordinates of corresponding reference points.
+    """Calculate homography matrix using pixel and world coordinates of corresponding 
+    reference points.
 
     Keyword arguments:
     refpts_pixel -- reference points in pixel coordinates
     refpts_world -- reference points in pixel coordinates
     """
 
-    # Upshift both x and y world coordinates of reference points to next round 500m value (UTM is in meters)
+    # Upshift both x and y world coordinates of reference points to next round 500m 
+    # value (UTM is in meters)
     min = np.amin(refpts_world, axis=0)
     max = np.amax(refpts_world, axis=0)
     mean = np.divide(np.add(min, max), 2)
@@ -159,43 +183,75 @@ def calculate_homography_matrix(refpts_pixel, refpts_world):
     # Truncate thousands digits from shifted reference points
     refpts_world_upshifted_postdecimal = np.mod(refpts_world_upshifted, 1)
     refpts_world_upshifted_predecimal = refpts_world_upshifted.astype(int)
-    refpts_world_upshifted_predecimal_pt1 = np.divide(refpts_world_upshifted_predecimal, 1000).astype(int)
-    refpts_world_upshifted_predecimal_pt1_1row = np.array([[refpts_world_upshifted_predecimal_pt1.item(0), refpts_world_upshifted_predecimal_pt1.item(1)]])
-    refpts_world_upshifted_predecimal_pt2 = np.mod(refpts_world_upshifted_predecimal, 1000)
-    refpts_world_upshifted_disassembled = np.add(refpts_world_upshifted_predecimal_pt2, refpts_world_upshifted_postdecimal)
+    refpts_world_upshifted_predecimal_pt1 = np.divide(
+        refpts_world_upshifted_predecimal, 1000
+    ).astype(int)
+    refpts_world_upshifted_predecimal_pt1_1row = np.array(
+        [
+            [
+                refpts_world_upshifted_predecimal_pt1.item(0),
+                refpts_world_upshifted_predecimal_pt1.item(1),
+            ]
+        ]
+    )
+    refpts_world_upshifted_predecimal_pt2 = np.mod(
+        refpts_world_upshifted_predecimal, 1000
+    )
+    refpts_world_upshifted_disassembled = np.add(
+        refpts_world_upshifted_predecimal_pt2, refpts_world_upshifted_postdecimal
+    )
 
-    # Calculate homography matrix with refpts in pixel coordinates and truncated & shifted refpts in world coordinates
-    homography_matrix, mask = cv2.findHomography(refpts_pixel, refpts_world_upshifted_disassembled, cv2.RANSAC, 3.0)  # RANSAC: Otulier/Inlier definieren??? # FEHLER:
+    # Calculate homography matrix with refpts in pixel coordinates and truncated &
+    # shifted refpts in world coordinates
+    homography_matrix, mask = cv2.findHomography(
+        refpts_pixel, refpts_world_upshifted_disassembled, cv2.RANSAC, 3.0
+    )  # RANSAC: Otulier/Inlier definieren??? # FEHLER:
     print(homography_matrix)
     return homography_matrix, refpts_world_upshifted_predecimal_pt1_1row, upshift_world
 
 
-def convertPixelToWorld(traj_pixel, homography_matrix, refpts_world_upshifted_predecimal_pt1_1row, upshift_world):
+def convertPixelToWorld(
+    traj_pixel,
+    homography_matrix,
+    refpts_world_upshifted_predecimal_pt1_1row,
+    upshift_world,
+):
     """Convert trajectories from pixel to world coordinates using homography matrix.
 
     Keyword arguments:
     traj_pixel -- Trajectory points in pixel coordinates
-    homography_matrix -- Homography matrix gathered from reference points in pixel and world coordinates
-    refpts_world_upshifted_predecimal_pt1_1row -- Thousands digits of reference points in world coorindates
+    homography_matrix -- Homography matrix gathered from reference points in pixel and
+    world coordinates
+    refpts_world_upshifted_predecimal_pt1_1row -- Thousands digits of reference points
+    in world coorindates
     upshift_world -- Formerly performed upshift of reference points coordinates
     """
 
-    # Transform pandas dataframe to numpy array, add 1 dimension and apply OpenCV´s perspective transformation
-    traj_pixel_np = traj_pixel[['x', 'y']].to_numpy(dtype='float32')
-    traj_pixel_np_tmp = np.array([traj_pixel_np], dtype='float32')
-    traj_world_upshifted_np_disassembled_3d = cv2.perspectiveTransform(traj_pixel_np_tmp, homography_matrix)
-    traj_world_upshifted_np_disassembled = np.squeeze(traj_world_upshifted_np_disassembled_3d)
+    # Transform pandas dataframe to numpy array, add 1 dimension and apply OpenCV´s
+    # perspective transformation
+    traj_pixel_np = traj_pixel[["x", "y"]].to_numpy(dtype="float32")
+    traj_pixel_np_tmp = np.array([traj_pixel_np], dtype="float32")
+    traj_world_upshifted_np_disassembled_3d = cv2.perspectiveTransform(
+        traj_pixel_np_tmp, homography_matrix
+    )
+    traj_world_upshifted_np_disassembled = np.squeeze(
+        traj_world_upshifted_np_disassembled_3d
+    )
 
     # Concatenate the thousands digits truncated before transformation
-    traj_world_upshifted_np = np.add(np.multiply(refpts_world_upshifted_predecimal_pt1_1row, 1000),
-                                    traj_world_upshifted_np_disassembled)
+    traj_world_upshifted_np = np.add(
+        np.multiply(refpts_world_upshifted_predecimal_pt1_1row, 1000),
+        traj_world_upshifted_np_disassembled,
+    )
 
-    # Shift back down both y and y world coordinates (same values as reference points were upshifted)
+    # Shift back down both y and y world coordinates (same values as reference points
+    # were upshifted)
     traj_world_np = np.subtract(traj_world_upshifted_np, upshift_world)
 
-    # In trajectory dataframe, overwrite pixel coordinates with world coordinates (from numpy array)
+    # In trajectory dataframe, overwrite pixel coordinates with world coordinates
+    # (from numpy array)
     traj_world = traj_pixel
-    traj_world[['x', 'y']] = traj_world_np
+    traj_world[["x", "y"]] = traj_world_np
 
     return traj_world
 
@@ -209,7 +265,9 @@ def save_traj_world(traj_pixel_path, traj_world):
     """
 
     traj_world.to_csv(traj_pixel_path[:-4] + "World_decDot.csv", index=False, sep=";")
-    traj_world.to_csv(traj_pixel_path[:-4] + "World_decComma.csv", index=False, sep=";", decimal=",")
+    traj_world.to_csv(
+        traj_pixel_path[:-4] + "World_decComma.csv", index=False, sep=";", decimal=","
+    )
     traj_world.to_pickle(traj_pixel_path[:-4] + "World.pkl")
 
 
@@ -217,18 +275,29 @@ def main():
     # Find homography matrix for corresponding refpts in pixel and world coordinates
     refpts_pixel = read_refpts_pixel_dialog()
     refpts_world = read_refpts_world_dialog()
-    homography_matrix, refpts_world_upshifted_predecimal_pt1_1row, upshift_world = calculate_homography_matrix(refpts_pixel, refpts_world)
+    (
+        homography_matrix,
+        refpts_world_upshifted_predecimal_pt1_1row,
+        upshift_world,
+    ) = calculate_homography_matrix(refpts_pixel, refpts_world)
 
-    # Convert trajectories from pixel to world coordinates using homography matrix for all selected trajectory files
+    # Convert trajectories from pixel to world coordinates using homography matrix for
+    # all selected trajectory files
     traj_pixel_paths = choose_traj_pixel_dialog()
     for traj_pixel_path in traj_pixel_paths:
         try:
             traj_pixel = read_traj_pixel_dialog(traj_pixel_path)
         except:
             print("Failed to read file: " + traj_pixel_path)
-        traj_world = convertPixelToWorld(traj_pixel, homography_matrix, refpts_world_upshifted_predecimal_pt1_1row, upshift_world)
+        traj_world = convertPixelToWorld(
+            traj_pixel,
+            homography_matrix,
+            refpts_world_upshifted_predecimal_pt1_1row,
+            upshift_world,
+        )
         save_traj_world(traj_pixel_path, traj_world)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+    
