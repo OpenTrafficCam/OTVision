a7aa2da374aeccff45462538c8ff21698acfe8b4
martinbaerwolff <maritn.baerwolff@tu-dresden.de>
2020-11-10 18:49:40 +0100

    Added input for title and file type/extension

--- OTVision/gui/folders_files.py
+++ OTVision/gui/folders_files.py
@@ -19,7 +19,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import PySimpleGUI as sg
-from PySimpleGUI.PySimpleGUI import LISTBOX_SELECT_MODE_EXTENDED, LISTBOX_SELECT_MODE_MULTIPLE
+from PySimpleGUI.PySimpleGUI import LISTBOX_SELECT_MODE_EXTENDED
 
 
 def drop_duplicates(list):
@@ -39,7 +39,44 @@ def drop_duplicates(list):
     return cleaned_list
 
 
-def main(title, filetype, input_folders=[], input_files=[]):
+def update_gui(window, folders, files, paths):
+    """
+    Updates elements of the folders/files picker gui below
+
+    Args:
+        window: pysimplegui window element
+        folders: current list of folders
+        files: current list of files
+        paths: current list of paths
+
+    Returns:
+        No returns
+    """
+    window["-text_folders-"].Update("Number of selected folders: " + str(len(folders)))
+    window["-text_files-"].Update("Number of selected files: " + str(len(files)))
+    window["-listbox_paths-"].Update(paths)
+
+
+def main(
+    title="Select folders/files",
+    file_extensions=(("All files", "."),),
+    input_folders=[],
+    input_files=[],
+):
+    """
+    Window for picking folders/files for further processing with OTVision tools
+
+    Args:
+        title: title of the window (default: "Select folders/files")
+        file_extensions: Tuple of tuple of file type and file extension
+        of the files to be selected (default: All files)
+        input_folders: list of pre-selected folders (default: empty list)
+        input_files: list of pre-selected files (default: empty list)
+
+    Returns:
+        folders: list of selected folders
+        files: list of selected files
+    """
     # Constants
     WIDTH_COL1 = 150
 
@@ -55,8 +92,7 @@ def main(title, filetype, input_folders=[], input_files=[]):
     paths.extend(folders)
     paths.extend(files)
 
-    # GUI elements: Trajectories
-    # header_traj = sg.Text("Provide trajectories")
+    # GUI elements ADD
     browse_folder = sg.FolderBrowse(
         "Add folder", key="-browse_folder-", target="-dummy_input_folder-",
     )
@@ -66,6 +102,18 @@ def main(title, filetype, input_folders=[], input_files=[]):
         enable_events=True,
         visible=False,
     )
+    browse_files = sg.FilesBrowse(
+        "Add file(s)",
+        key="-browse_files-",
+        target="-dummy_input_files-",
+        enable_events=True,
+        file_types=file_extensions,
+    )
+    dummy_input_files = sg.Input(
+        key="-dummy_input_files-", enable_events=True, visible=False
+    )
+
+    # GUI elements STATUS
     text_listbox_paths = sg.Text("Selected folders and files:")
     listbox_paths = sg.Listbox(
         values=paths,
@@ -74,32 +122,25 @@ def main(title, filetype, input_folders=[], input_files=[]):
         enable_events=True,
         select_mode=LISTBOX_SELECT_MODE_EXTENDED,
     )
-    browse_files = sg.FilesBrowse(
-        "Add file(s)",
-        key="-browse_files-",
-        target="-dummy_input_files-",
-        enable_events=True,
-    )
-    dummy_input_files = sg.Input(
-        key="-dummy_input_files-", enable_events=True, visible=False
-    )
     text_folders = sg.Text(
-        str(len(folders)) + " folders selected.",
+        "Number of selected folders: " + str(len(folders)),
         key="-text_folders-",
         size=(WIDTH_COL1, 1),
     )
     text_files = sg.Text(
-        str(len(files)) + " files selected.", key="-text_files-", size=(WIDTH_COL1, 1),
+        "Number of selected files: " + str(len(files)),
+        key="-text_files-",
+        size=(WIDTH_COL1, 1),
     )
+
+    # GUI elements REMOVE
     button_remove_selection = sg.Button(
         "Remove selection", key="-button_remove_selection-"
     )
-    button_keep_selection = sg.Button(
-        "Keep selection", key="-button_keep_selection-"
-    )
+    button_keep_selection = sg.Button("Keep selection", key="-button_keep_selection-")
     button_remove_all = sg.Button("Remove all", key="-button_remove_all-")
 
-    # GUI elements: Exit gui data
+    # GUI elements QUIT
     button_ok = sg.Button("Ok", key="-button_ok-")
     button_cancel = sg.Button("Cancel", key="-button_cancel-")
 
@@ -117,8 +158,7 @@ def main(title, filetype, input_folders=[], input_files=[]):
     ]
 
     # Create the Window
-    window_title = "Transform trajectories from pixel to world coordinates"
-    window = sg.Window(window_title, layout).Finalize()
+    window = sg.Window(title, layout).Finalize()
 
     # Make window fullscreen
     window.Maximize()
@@ -136,55 +176,45 @@ def main(title, filetype, input_folders=[], input_files=[]):
             folders.append(values["-dummy_input_folder-"])
             window["-dummy_input_folder-"].Update("")
             folders = drop_duplicates(folders)
-            if len(folders) == 1:
-                text_folders_label = " folder selected."
-            else:
-                text_folders_label = " folders selected."
-            window["-text_folders-"].Update(str(len(folders)) + text_folders_label)
             paths.extend(folders)
             paths = drop_duplicates(paths)
-            window["-listbox_paths-"].Update(paths)
+            update_gui(window, folders, files, paths)
         elif event == "-dummy_input_files-" and values["-dummy_input_files-"] != "":
             files.extend(values["-dummy_input_files-"].split(";"))
             window["-dummy_input_files-"].Update("")
             files = drop_duplicates(files)
-            if len(files) == 1:
-                text_files_label = " file selected."
-            else:
-                text_files_label = " files selected."
-            window["-text_files-"].Update(str(len(files)) + text_files_label)
             paths.extend(files)
             paths = drop_duplicates(paths)
-            window["-listbox_paths-"].Update(paths)
+            update_gui(window, folders, files, paths)
         elif event == "-button_remove_all-":
             folders = []
             files = []
             paths = []
-            window["-text_folders-"].Update(str(len(folders)) + " folders selected.")
-            window["-text_files-"].Update(str(len(files)) + " files selected.")
-            window["-listbox_paths-"].Update(paths)
+            update_gui(window, folders, files, paths)
         elif event == "-listbox_paths-":
             selected_paths = values["-listbox_paths-"]
-            print(selected_paths)
         elif event == "-button_keep_selection-":
             folders = [i for i in folders if i in selected_paths]
             files = [i for i in files if i in selected_paths]
             paths = selected_paths
             window["-text_folders-"].Update(str(len(folders)) + " folders selected.")
             window["-text_files-"].Update(str(len(files)) + " files selected.")
-            window["-listbox_paths-"].Update(paths)
+            update_gui(window, folders, files, paths)
         elif event == "-button_remove_selection-":
             folders = [i for i in folders if i not in selected_paths]
             files = [i for i in files if i not in selected_paths]
             paths = [i for i in paths if i not in selected_paths]
-            window["-text_folders-"].Update(str(len(folders)) + " folders selected.")
-            window["-text_files-"].Update(str(len(files)) + " files selected.")
-            window["-listbox_paths-"].Update(paths)
+            update_gui(window, folders, files, paths)
 
 
 if __name__ == "__main__":
     print(
-        main("Select images", ".jpg", ["Input_Test_Folder"], ["Input_Test_File.ending"])
+        main(
+            title="Select images",
+            file_extensions=(("Images", ".jpg"), ("Images", ".png")),
+            input_folders=["Input_Test_Folder"],
+            input_files=["Input_Test_File.ending"],
+        )
     )
 
 
