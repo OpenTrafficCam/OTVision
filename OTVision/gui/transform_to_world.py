6acf667371c675f31afe93b9de6557303516169e
martinbaerwolff <maritn.baerwolff@tu-dresden.de>
2020-11-11 03:07:43 +0100

    Further worked on transform_to_world gui

--- OTVision/gui/transform_to_world.py
+++ OTVision/gui/transform_to_world.py
@@ -19,21 +19,49 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+from tkinter.font import BOLD
 import PySimpleGUI as sg
 from gui import browse_folders_and_files
+from gui.sg_otc_theme import OTC_ICON, OTC_BUTTON, OTC_THEME
+import cv2
+import time
 
 
-def main():
-    # Constants
-    WIDTH_COL1 = 150
+# Constants
+WIDTH_COL1 = 150
 
+
+def create_window(OTC_ICON, layout):
+    window_title = "Transform trajectories from pixel to world coordinates"
+    window = (
+        sg.Window(window_title, icon=OTC_ICON, resizable=True)
+        .Layout(
+            [
+                [
+                    sg.Column(
+                        layout=layout,
+                        key="-column-",
+                        scrollable=True,
+                        vertical_scroll_only=False,
+                        expand_x=True,
+                        expand_y=True,
+                    )
+                ]
+            ]
+        )
+        .Finalize()
+    )
+    return window
+
+
+def main(sg_theme=OTC_THEME):
     # Lists
     # traj_paths = []
     traj_folders = []
     traj_files = []
 
     # GUI elements: Trajectories
-    header_traj = sg.Text("Provide trajectories")
+    header_traj = sg.Text("Step 1: Provide trajectories", font=BOLD)
     button_browse_traj = sg.Button(
         "Browse trajectory files", key="-button_browse_traj-"
     )
@@ -50,24 +78,59 @@ def main():
 
     # GUI elemnts: Reference points
     header_refpts = sg.Text(
-        "Provide reference points in both pixel and world coordinates"
+        "Step 2: Provide reference points in both pixel and world coordinates", font=BOLD
     )
-    button_click_refpts = sg.Button(
-        "Click new reference points", key="-button_click_refpts-"
+    input_refpts = sg.In(
+        key="-input_refpts-", size=(WIDTH_COL1, 1), enable_events=True, visible=True,
     )
-    dummy_input_refpts_path = sg.In(
-        key="-dummy_input_refpts_path-", enable_events=True, visible=False
-    )
-    browse_refpts_path = sg.FileBrowse(
+    browse_refpts = sg.FileBrowse(
         "Choose existing reference points",
-        key="-browse_refpts_path-",
-        target="-dummy_input_refpts_path-",
+        key="-browse_refpts-",
+        target="-input_refpts-",
         enable_events=True,
+    )
+    filesaveas_refpts = sg.FileSaveAs(
+        "Save reference points as ...", key="-filesaveas_refpts-"
+    )
+
+    # GUI elements: transform to world
+    header_transform = sg.Text("Step 3: Start transformation to world coordinates", font=BOLD)
+
+    # Video properties
+    video = cv2.VideoCapture(
+        r"C:\Users\Baerwolff\Desktop\Lenovo_Arbeit\2020-02-20_Validierungsmessung_Radeberg\raspberrypi_FR20_2020-02-20_11-30-00.flv"
+    )
+    video_width = video.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+    video_height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+    video_fps = video.get(cv2.CAP_PROP_FPS)
+    video_total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    a_id = None
+    play_video = False
+    ret = False
+    video_curr_frame = 0
+
+    # Gui elements: Video player
+    callback_image = sg.Graph(
+        (video_width, video_height),
+        (0, video_height),
+        (video_width, 0),
+        key="-callback_image-",
+        enable_events=True,
+        drag_submits=True,
+        background_color="black",
+    )
+    button_play = sg.Button("Play", key="-button_play-")
+    button_pause = sg.Button("Pause", key="-button_pause-")
+    slider_video = sg.Slider(
+        range=(0, video_total_frames),
+        size=(60, 10),
+        orientation="h",
+        key="-slider_video-",
     )
 
     # GUI elements: Exit gui data
     button_back_to_otvision = sg.Button(
-        "Back to OTVision", key="-button_back_to_otvision-"
+        "", key="-button_back_to_otvision-", image_data=OTC_BUTTON, border_width=0,
     )
 
     # All the stuff inside the window
@@ -76,31 +139,65 @@ def main():
         [button_browse_traj],
         [text_traj_folders],
         [text_traj_files],
+        [sg.Text("")],
         [header_refpts],
-        [button_click_refpts, dummy_input_refpts_path, browse_refpts_path],
+        [browse_refpts, input_refpts],
+        [callback_image],
+        [button_play, button_pause],
+        [slider_video],
+        [filesaveas_refpts],
+        [sg.Text("")],
+        [header_transform],
+        [sg.Text("")],
         [button_back_to_otvision],
     ]
 
     # Create the Window
-    window_title = "Transform trajectories from pixel to world coordinates"
-    window = sg.Window(window_title, layout).Finalize()
-
-    # Make window fullscreen
-    window.Maximize()
+    window = create_window(OTC_ICON, layout)
 
     # Event Loop to process "events" and get the "values" of the inputs
-    while True:
-        event, values = window.read()
+    while video.isOpened():
+        event, values = window.read(timeout=0)
+        window["-column-"].expand(True, True)
+        # screen_width, screen_height = window.get_screen_size()
         if (
             event == sg.WIN_CLOSED
             or event == "Cancel"
             or event == "-button_back_to_otvision-"
         ):  # if user closes window or clicks cancel
             break
+        # Video stuff
+        if event == "-button_play-":
+            play_video = True
+        elif event == "-button_pause-" or not ret:
+            play_video = False
+        if play_video:
+            ret, frame = video.read()
+            imgbytes = cv2.imencode(".png", frame)[1].tobytes()
+            if a_id:
+                window["-callback_image-"].delete_figure(a_id)  # delete previous image
+            a_id = window["-callback_image-"].draw_image(
+                data=imgbytes, location=(0, 0)
+            )  # draw new image
+            window["-callback_image-"].TKCanvas.tag_lower(a_id)  # move image to bottom
+            time.sleep(1 / video_fps)
+            # if someone moved the slider manually, the jump to that frame
+            if int(values["-slider_video-"]) != video_curr_frame - 1:
+                video_curr_frame = int(values["-slider_video-"])
+                video.set(cv2.CAP_PROP_POS_FRAMES, video_curr_frame)
+            window["-slider_video-"].update(video_curr_frame)
+            # video_curr_frame += 1
+        # Video callbacks
+        if event == "-callback_image-":
+            refpts_px_values = values["-callback_image-"]
+            window["-callback_image-"].draw_circle(
+                refpts_px_values, 5, fill_color="red", line_color="red"
+            )
+        # Other gui stuff
         if event == "-button_browse_traj-":
             traj_folders, traj_files = browse_folders_and_files.main(
                 title="Select trajectories",
-                filetype="json",
+                filetype="_trackspx.json",
                 input_folders=traj_folders,
                 input_files=traj_files,
             )
@@ -139,7 +236,16 @@ def main():
     window.close()
 
 
-# To Dos
-# - Code "clear selection" button, which lists traj_folders and traj_files
-#   and updates text
-# - Remove duplicates from lists traj_folders and traj_files instantly after browsing
+# To Dos:
+# - Rearange loop to enable Play, Pause and Slider simultaneously
+# - Load video automatically by file name of "[...]_trackspx.json"
+#   or otherwise by browse button
+# - Remove error when video runs out of frames
+# - Draw on every mouse click (everey now and then it doesnt work)
+# - Draw crosshair instead of circle
+# - Add embededding or popup for map
+# - Append clicked pixel and utm refpts to dictionary
+# - Button to load/save this dictionary from/to json/pkl/npy/txt
+# - Button to call transfrom_to_world function passing refpts
+# - Add function for accuracy evaluation to transform_to_world
+# - Some element to show results of accuracy evaluation
