91399e0a79393ed17b0cabd90cd9dd7d55a80f22
martinbaerwolff <53005443+martinbaerwolff@users.noreply.github.com>
2020-11-11 16:39:39 +0100

    Merge latest changes from master into development branch (#27)
    * Inserted code from own repo
    
    * added workflow for linter (#12)
    
    * Changed to meet naming conventions
    
    * Changed comments to PEP8 standard
    
    * Added header with  module and license info
    
    * Changed default folder to test data folder of repo
    
    * Fixed PEP-8 line length violations
    
    * Added Radeberg test data to repo
    
    * Updated relative path to test data folder
    
    * Added transfomation accuracy evaluation
    
    * Added test data
    
    * Updated linting settings
    
    * added flake8
    
    * Outsourcing code for readability and reusability
    
    * Restrucured package
    
    * Added gui for transformation to world coordinates
    
    * Added cpython-37.pyc-files to gitignore file
    
    * added vscode settings (#17)
    
    * added vscode settings
    
    * added pylance as language server
    
    * Kept working on t2w gui
    
    * Revert "Inserted code from own repo" (#22)
    
    * added get_files() (#24)
    
    * added vscode settings (#17) (#18)
    
    * added vscode settings
    
    * added pylance as language server
    
    * added get_files()
    
    * add_folders_and_files (#25)
    
    * Added gui for browsing folders and files
    
    * Deleted comments at end of module
    
    Co-authored-by: martinbaerwolff <maritn.baerwolff@tu-dresden.de>
    
    Co-authored-by: martinbaerwolff <maritn.baerwolff@tu-dresden.de>
    Co-authored-by: Sebastian Buck <38660441+frunika@users.noreply.github.com>

---
+++ OTVision/helpers/files.py
@@ -0,0 +1,83 @@
+# OTVision: helpers for filehandling
+# Copyright (C) 2020 OpenTrafficCam Contributors
+# <https://github.com/OpenTrafficCam
+# <team@opentrafficcam.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+
+def get_files(paths, filetype):
+    """
+    Generates a list of files ending with filename based on filenames or the recursive
+    contend of folders.
+
+    Args:
+        paths ([str or list of str]): where to find the files
+        filetype ([str]): ending of the files to find. Preceding "_" prevents adding a '.'
+
+    Returns:
+        [list]: [list of filenames as str]
+    """
+
+    files = set()
+    pathlist = []
+
+    # check, if _paths_ is a list or str
+    if type(paths) is list:
+        pathlist.extend(paths)
+    elif type(paths) is str:
+        pathlist.append(paths)
+    else:
+        raise TypeError("Paths needs to be str or list of str")
+
+    # check if _filename_ is str and transform it
+    if type(filetype) is str:
+        if not filetype.startswith("_"):
+            if not filetype.startswith("."):
+                filetype = "." + filetype
+            filetype = filetype.lower()
+    else:
+        raise TypeError("filetype needs to be a str")
+
+    # add all files to a single list _files_
+    for path in pathlist:
+        path = Path(path)
+        if path.is_file():
+            file = str(path)
+            if file.endswith(filetype):
+                files.add(file)
+        elif path.is_dir():
+            for file in path.glob("**/*" + filetype):
+                file = str(file)
+                files.add(file)
+
+    return sorted(list(files))
+
+
+if __name__ == "__main__":
+    paths = "D:/tmp/"
+    # paths = ["D:/tmp/tmp1", "D:\\tmp\\tmp2"]
+    # paths = ["D:/tmp/tmp1/", "D:\\tmp/tmp2\\", "D:/tmp/test_objects.csv"]
+    # paths = "D:/tmp/test_objects.csv"
+
+    # filetype = "csv"
+    # filetype = "CSV"
+    # filetype = "_objects.csv"
+    filetype = ".csv"
+
+    files = get_files(paths, filetype)
+    for file in files:
+        print(file)
