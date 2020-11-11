3136b59de61803b795d5b61e2cebd882978d3392
martinbaerwolff <maritn.baerwolff@tu-dresden.de>
2020-11-11 03:12:13 +0100

    Added license

---
+++ OTVision/gui/__init__.py
@@ -0,0 +1,19 @@
+# OTVision: Python module to calculate homography matrix from reference
+# points and transform trajectory points from pixel into world coordinates.
+
+# Copyright (C) 2020 OpenTrafficCam Contributors
+# <https://github.com/OpenTrafficCam
+# <team@opentrafficcam.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
