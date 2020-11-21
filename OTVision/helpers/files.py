b04bdf700f749a46ec7f43835837f12419e25b0d
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2020-11-21 16:22:27 +0100

    function to remove a dir and all content (#33)

--- OTVision/helpers/files.py
+++ OTVision/helpers/files.py
@@ -67,6 +67,16 @@ def get_files(paths, filetype):
     return sorted(list(files))
 
 
+def remove_dir(dir: str):
+    dir = Path(dir)
+    for path in dir.glob("*"):
+        if path.is_file():
+            path.unlink()
+        else:
+            remove_dir(path)
+    dir.rmdir()
+
+
 if __name__ == "__main__":
     paths = "D:/tmp/"
     # paths = ["D:/tmp/tmp1", "D:\\tmp\\tmp2"]
