ce921036f9d4c9014cf7ccaf8e55f5bc1ad54bf4
martinbaerwolff <53005443+martinbaerwolff@users.noreply.github.com>
2021-03-20 19:39:47 +0100

    Distinguish between input and output frame rate

--- OTVision/convert/convert.py
+++ OTVision/convert/convert.py
@@ -1,18 +1,24 @@
-import cv2
 import re
 import os
 from pathlib import Path
 
-FFMPEG_PATH = Path(__file__).parents[0] / r"ffmpeg.exe"
 
-
-def convert(input_video, output_extension=".avi", output_fps=None, overwrite=True):
+def convert(
+    input_video: str,
+    output_filetype: str = ".avi",
+    input_fps: float = None,
+    output_fps: float = None,
+    overwrite: bool = True,
+):
+    FFMPEG_PATH = Path(__file__).parents[0] / r"ffmpeg.exe"
+    DEFAULT_FPS = 25.0
     input_path = Path(input_video)
     input_filename = input_path.stem
-    output_path = input_path.with_suffix(output_extension)
+    input_filetype = input_path.suffix
+    output_path = input_path.with_suffix(output_filetype)
     if not overwrite and output_path.is_file:
         return None
-    vid_extensions = [
+    vid_filetypes = [
         ".mov",
         ".avi",
         ".mp4",
@@ -23,25 +29,44 @@ def convert(input_video, output_extension=".avi", output_fps=None, overwrite=Tru
         ".mkv",
         ".h264",
     ]
-    try:
-        input_fps = float(re.search("_FR(.*?)_", input_filename).group(1))
-    except AttributeError:
-        try:
-            input_fps = cv2.VideoCapture(input_video).get(cv2.CAP_PROP_FPS)
-        except:
-            input_fps = None
-    if output_fps is None:
-        if input_fps is None:
-            output_fps = 25.0
+    if input_filetype in vid_filetypes and output_filetype in vid_filetypes:
+        if input_filetype == ".h264" and input_fps is None:
+            try:
+                # Get input fps frome filename
+                input_fps = float(re.search("_FR(.*?)_", input_filename).group(1))
+            except AttributeError("Frame rate not found in filename"):
+                input_fps = DEFAULT_FPS
+
+        print(f"Input fps: {input_fps}")
+        print(f"Output fps: {output_fps}")
+
+        # Create ffmpeg command
+        if input_fps is not None:
+            input_fps_cmd = f"-framerate {input_fps}"
         else:
-            output_fps = input_fps
-
-    print(f"input fps: {input_fps}")
-    print(f"output fps: {output_fps}")
-
-    if input_path.suffix in vid_extensions and output_path.suffix in vid_extensions:
-        ffmpeg_cmd = f"{FFMPEG_PATH} -r {str(output_fps)} -i {input_path} -vcodec copy -an -y {output_path}"
+            input_fps_cmd = ""
+        if output_fps is not None:
+            output_fps_cmd = f"-r {output_fps}"
+            copy_cmd = ""
+        else:
+            output_fps_cmd = ""
+            copy_cmd = "-vcodec copy"  # No decoding, only demuxing
+        # Input file
+        ffmpeg_cmd_in = f"{input_fps_cmd} -i {input_path}"
+        # Filters
+        ffmpeg_cmd_filter = f"-c:v libx264 {output_fps_cmd} {copy_cmd}"
+        # Output file
+        ffmpeg_cmd_out = f"-y {output_path}"
+        # Concat and run ffmpeg command
+        ffmpeg_cmd = (
+            f"{FFMPEG_PATH} {ffmpeg_cmd_in} {ffmpeg_cmd_filter} {ffmpeg_cmd_out}"
+        )
         os.system(ffmpeg_cmd)
+
+    elif input_filetype in vid_filetypes:
+        raise TypeError("Output video filetype is not supported")
+    elif output_filetype in vid_filetypes:
+        raise TypeError("Input video filetype is not supported")
 
 
 if __name__ == "__main__":
@@ -50,4 +75,12 @@ if __name__ == "__main__":
         Path(__file__).parents[2]
         / r"tests/data/testvideo_FR20_2020-02-20_12-00-00.h264"
     )
-    convert(input_video=test_video, output_extension=".avi")
+    convert(input_video=test_video, output_filetype=".avi")
+
+# Useful ffmpeg commands:
+# "-vcodec"=Get video only
+# "copy"=only demuxing and muxing
+# "-bsf:v h264_mp4toannexb"
+# "-c:v libx264"= encoder for h264 input stream
+# "-bsf:v h264_mp4toannexb"=?
+# "-y"=Overwrite output file
