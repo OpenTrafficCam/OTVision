5637f86e82aaa53f329d5f2bc706319ae84b5837
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2021-04-10 19:40:42 +0200

    Add convert.py to package root for testing

---
+++ OTVision/convert.py
@@ -0,0 +1,31 @@
+# OTVision: Python module to testwise run OTVision/detect/detect.py
+
+# Copyright (C) 2020 OpenTrafficCam Contributors
+# <https://github.com/OpenTrafficCam
+# <team@opentrafficcam.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from pathlib import Path
+from convert.convert import main as convert
+
+
+if __name__ == "__main__":
+    convert(
+        str(
+            Path(__file__).parents[1]
+            / r"tests/data/testvideo_FR20_2020-02-20_12-00-00.h264"
+        )
+    )
