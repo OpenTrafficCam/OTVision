4aa4b37a97febc69d8068544b5af0d6a2d150275
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2021-06-07 16:35:20 +0200

    [TEMP][api_test] conform to input requirement of track.write()

--- tests/api_test.py
+++ tests/api_test.py
@@ -21,7 +21,10 @@ new_detections, tracks_finished, vehIDs_finished = iou.track_iou(
     detections["data"], t_min=0, sigma_h=0.5
 )
 
-# %%
+# TODO: This should probably be fixed otherwise, such that the output
+#       data of track_iou is harmonized with the input of track.write()
+#       and that wrapping of new_detections into a data field is obsolete
+new_detections = {"data": new_detections}
 track.write(new_detections, Path(detections_file).with_suffix(".ottrk"))
 
 # %%
