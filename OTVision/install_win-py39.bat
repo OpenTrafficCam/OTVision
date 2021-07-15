f801abcf310c0b6381d6613b74dbd9c2547990af
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2021-07-15 22:08:37 +0200

    Split requirements.txt and install.bat by py ver

--- OTVision/install_win-py39.bat
+++ OTVision/install_win-py39.bat
@@ -2,6 +2,6 @@ cd ..
 python -m venv venv
 call venv/Scripts/activate
 REM maybe "set GDAL_VERSION=3.2.3" is needed
-pip install -r requirements.txt
+pip install -r requirements_win-py39.txt
 deactivate
 pause
