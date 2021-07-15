bb33473e8704c15a5ce1cee4cc249fe77314a57e
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2021-07-15 23:51:08 +0200

    Add timout and change direction of slashes

--- OTVision/install_win-py38.bat
+++ OTVision/install_win-py38.bat
@@ -1,7 +1,9 @@
 cd ..
 python -m venv venv
-call venv/Scripts/activate
+call venv\Scripts\activate
+timeout /T 1
 REM maybe "set GDAL_VERSION=3.2.3" is needed
 pip install -r requirements_win-py38.txt
+timeout /T 10
 deactivate
 pause
