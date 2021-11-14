2db23b203ea2764a52d063cc234b2793b01147c9
Randy Seng <rseng@pm.me>
2021-11-14 19:29:16 +0100

    Declare interface for OTVision

---
+++ OTVision/__init__.py
@@ -0,0 +1,6 @@
+# TODO: Might need to change this
+from .detect.detect import main as detect
+from .track.track import main as track
+from .convert.convert import main as convert
+from .gui import __main__ as gui
+from .gui.transform_view import main
