37509c0935db8b03cadf946ffd02714f4f86057c
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2022-02-17 22:02:16 +0100

    prevent pip from installing cached packages

--- install_win-py38.bat
+++ install_win-py38.bat
@@ -2,7 +2,7 @@ python -m venv venv
 call venv\Scripts\activate
 timeout /T 1
 REM maybe "set GDAL_VERSION=3.2.3" is needed
-pip install -r requirements_win-py38.txt
+pip install -r requirements_win-py38.txt --no-cache-dir
 timeout /T 10
 deactivate
 pause
