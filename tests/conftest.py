035fc09e35e74fb8e69ef64d6ef048c13e15d843
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2022-11-25 12:30:16 +0100

    type hint return Generator

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import shutil
+from collections.abc import Generator
 from pathlib import Path
 
 import pytest
 
 
 @pytest.fixture
-def test_data_tmp_dir() -> Path:
+def test_data_tmp_dir() -> Generator[Path, None, None]:
     test_data_tmp_dir = Path(__file__).parent / "data_tmp"
     test_data_tmp_dir.mkdir(exist_ok=True)
     yield test_data_tmp_dir
