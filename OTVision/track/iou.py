9209515b6f1428ee1aa94573f6ac078dcdceaa1f
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-01-26 13:07:37 +0100

    Remove unused import

--- OTVision/track/iou.py
+++ OTVision/track/iou.py
@@ -23,8 +23,6 @@ OTVision module to track road users in frames detected by OTVision
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-import pandas as pd
-
 from OTVision.config import CONFIG
 
 from .iou_util import iou
