1dc967f70e207491360b49860c629e06ed448274
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-02-02 19:45:01 +0100

    Add information message when starting install shell scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+echo "Install OTVision."
 
 WORKING_DIR=$(pwd)
 VENV="$WORKING_DIR"/venv
@@ -10,4 +11,3 @@ python3.10 -m venv "$VENV"
 
 $PYTHON -m pip install --upgrade pip
 $PIP install -r requirements.txt --no-cache-dir
-source "$VENV"/bin/activate
--- install_dev.sh
+++ install_dev.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
+echo "Install OTVision development environment."
 
 WORKING_DIR=$(pwd)
 VENV="$WORKING_DIR"/venv
-PYTHON="$VENV"/bin/python
 PIP="$VENV"/bin/pip
 PRE_COMMIT="$VENV"/bin/pre-commit
 
 bash "$WORKING_DIR"/install.sh
 
 $PIP install -r requirements_dev.txt --no-cache-dir
-$PRE_COMMIT install
-source "$VENV"/bin/activate
+$PRE_COMMIT install --install-hooks
