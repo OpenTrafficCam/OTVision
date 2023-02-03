c9b797af6757c408ad726f3d96a7b65816f0b310
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-02-03 16:55:35 +0100

    Own sub dir for tracks test data

--- tests/track/test_track.py
+++ tests/track/test_track.py
@@ -13,33 +13,41 @@ def test_track_pass(test_data_tmp_dir: Path, test_data_dir: Path) -> None:
     using the IOU tracker by Bochinski et al.
     """
 
+    # Define sub dirs and create tmp folder
+    test_tracks_dir = test_data_dir / "track"
+    test_tracks_tmp_dir = test_data_tmp_dir / "track"
+    test_tracks_tmp_dir.mkdir(exist_ok=True)
+
     # Get reference data
     ref_detections_files = get_files(
-        paths=[test_data_dir], filetypes=[CONFIG["DEFAULT_FILETYPE"]["DETECT"]]
+        paths=[test_tracks_dir],
+        filetypes=[CONFIG["DEFAULT_FILETYPE"]["DETECT"]],
     )
     ref_tracks_files = get_files(
-        paths=[test_data_dir], filetypes=[CONFIG["DEFAULT_FILETYPE"]["TRACK"]]
+        paths=[test_tracks_dir], filetypes=[CONFIG["DEFAULT_FILETYPE"]["TRACK"]]
     )
 
     # Copy input data to temporary folder and start conversion
     for ref_detections_file in ref_detections_files:
+        # test_detections_file = test_data_tmp_dir / "track" / ref_detections_file.name
+        test_detections_file = test_tracks_tmp_dir / ref_detections_file.name
         shutil.copy2(
-            Path(ref_detections_file),
-            Path(ref_detections_file).parents[1]
-            / test_data_tmp_dir.name
-            / Path(ref_detections_file).name,
+            ref_detections_file,
+            test_detections_file,
         )
-    test_detections_file = get_files(
-        paths=[test_data_tmp_dir],
-        filetypes=[CONFIG["DEFAULT_FILETYPE"]["DETECT"]],
-    )
 
     # Track all test detections files
-    track(test_detections_file)
+    dir_to_track = test_tracks_tmp_dir
+    track(paths=[dir_to_track])
 
     # Compare all test tracks files to their respective reference tracks files
     tracks_file_names = [file.name for file in ref_tracks_files]
     equal_files, different_files, irregular_files = cmpfiles(
-        a=test_data_dir, b=test_data_tmp_dir, common=tracks_file_names, shallow=False
+        a=test_tracks_dir,
+        b=test_tracks_tmp_dir,
+        common=tracks_file_names,
+        shallow=False,
     )
     assert tracks_file_names == equal_files
+    assert not different_files
+    assert not irregular_files
