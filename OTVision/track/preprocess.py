8d0a9ca9a5c0ac06bc78dcaf178f350e9e2083d6
Lars Briem <lars.briem@platomo.de>
2023-02-07 09:27:40 +0100

    Extract constants

--- OTVision/track/preprocess.py
+++ OTVision/track/preprocess.py
@@ -3,6 +3,17 @@ from pathlib import Path
 
 from OTVision.helpers.files import read_json
 
+DATA: str = "data"
+CLASS: str = "class"
+CLASSIFIED: str = "classified"
+FRAME: str = "frame"
+LABEL: str = "label"
+CONFIDENCE: str = "conf"
+X: str = "x"
+Y: str = "y"
+W: str = "W"
+H: str = "h"
+
 
 @dataclass(frozen=True, repr=True)
 class Detection:
@@ -20,13 +31,13 @@ class Detection:
 
     def to_dict(self) -> dict:
         return {
-            "frame": self.frame,
-            "label": self.label,
-            "conf": self.conf,
-            "x": self.x,
-            "y": self.y,
-            "w": self.w,
-            "h": self.h,
+            FRAME: self.frame,
+            LABEL: self.label,
+            CONFIDENCE: self.conf,
+            X: self.x,
+            Y: self.y,
+            W: self.w,
+            H: self.h,
         }
 
 
@@ -45,7 +56,7 @@ class Cleanup:
         """
         keys_to_drop = []
         for key, value in data.items():
-            classified_data = value["classified"]
+            classified_data = value[CLASSIFIED]
             if 0 == len(classified_data):
                 keys_to_drop.append(key)
         [data.pop(key) for key in keys_to_drop]
@@ -56,16 +67,16 @@ class DetectionParser:
     def convert(self, input: dict[str, dict[str, list]]) -> list[Detection]:
         detections: list[Detection] = []
         for key, value in input.items():
-            data_detections = value["classified"]
+            data_detections = value[CLASSIFIED]
             for detection in data_detections:
                 detected_item = Detection(
                     int(key),
-                    detection["class"],
-                    detection["conf"],
-                    detection["x"],
-                    detection["y"],
-                    detection["w"],
-                    detection["h"],
+                    detection[CLASS],
+                    detection[CONFIDENCE],
+                    detection[X],
+                    detection[Y],
+                    detection[W],
+                    detection[H],
                 )
                 detections.append(detected_item)
         return detections
@@ -74,6 +85,6 @@ class DetectionParser:
 class Preprocess:
     def load_data(self, input_path: Path) -> list[Detection]:
         input = read_json(input_path)
-        data: dict[str, dict[str, list]] = input["data"]
+        data: dict[str, dict[str, list]] = input[DATA]
         cleaned = Cleanup().remove_empty_frames(data)
         return DetectionParser().convert(cleaned)
