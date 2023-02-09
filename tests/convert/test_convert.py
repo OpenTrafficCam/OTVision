8bb8895a61b07b22098a78fc13daeb545a0f9c1d
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-02-09 11:33:00 +0100

    Move convert test data to own subdir

--- tests/convert/test_convert.py
+++ tests/convert/test_convert.py
@@ -1,13 +1,42 @@
 import shutil
 from pathlib import Path
+from typing import Generator, TypeVar
 
 import cv2
 import numpy as np
-from pytest import approx
+import pytest
 
+from OTVision.config import CONFIG
 from OTVision.convert.convert import check_ffmpeg
 from OTVision.convert.convert import main as convert
-from OTVision.helpers.files import get_files
+
+T = TypeVar("T")
+YieldFixture = Generator[T, None, None]
+
+
+@pytest.fixture
+def test_convert_dir(test_data_dir: Path) -> Path:
+    return test_data_dir / "convert"
+
+
+@pytest.fixture
+def test_convert_tmp_dir(
+    test_data_tmp_dir: Path, test_convert_dir: Path
+) -> YieldFixture[Path]:
+    # Create tmp dir
+    test_convert_tmp_dir = test_data_tmp_dir / "convert"
+    test_convert_tmp_dir.mkdir(exist_ok=True)
+    # Copy test files to tmp dir
+    shutil.copytree(test_convert_dir, test_convert_tmp_dir, dirs_exist_ok=True)
+    # Delete video files from tmp dir (we create them during the tests)
+    extension = CONFIG["DEFAULT_FILETYPE"]["VID"]
+    video_files_to_delete = test_convert_tmp_dir.rglob(f"*{extension}")
+    for f in video_files_to_delete:
+        f.unlink()
+    # Yield tmp dir
+    yield test_convert_tmp_dir
+    # Teardown tmp dir after use in test
+    shutil.rmtree(test_convert_tmp_dir)
 
 
 def test_check_ffmpeg() -> None:
@@ -15,29 +44,14 @@ def test_check_ffmpeg() -> None:
     check_ffmpeg()
 
 
-def test_convert(test_data_tmp_dir: Path, test_data_dir: Path) -> None:
+def test_convert(test_convert_dir: Path, test_convert_tmp_dir: Path) -> None:
     """Tests the main function of OTVision/convert/convert.py
     transforming short test videos from h264 to mp4 based on
     framerate specified as part of the file path using ffmpeg.exe
     """
 
-    # Get reference data
-    h264_ref_videos = get_files(paths=[test_data_dir], filetypes=[".h264"])
-    mp4_ref_videos = get_files(paths=[test_data_dir], filetypes=[".mp4"])
-
-    # Copy input data to temporary folder and start conversion
-    for h264_ref_video in h264_ref_videos:
-        shutil.copy2(
-            Path(h264_ref_video),
-            Path(h264_ref_video).parents[1]
-            / test_data_tmp_dir.name
-            / Path(h264_ref_video).name,
-        )
-    h264_test_videos = get_files(
-        paths=[test_data_tmp_dir],
-        filetypes=[".h264"],
-    )
-    convert(h264_test_videos)
+    # # Convert test h264 without further arguments
+    convert(paths=[test_convert_tmp_dir])
 
     # Local function to turn videos into np arrays for comparison
     def array_from_video(file: Path) -> np.ndarray:
@@ -61,9 +75,12 @@ def test_convert(test_data_tmp_dir: Path, test_data_dir: Path) -> None:
                 frames.append(img)
         return np.stack(frames, axis=0)
 
-    for mp4_ref_video in mp4_ref_videos:
+    # Get reference video files
+    extension = CONFIG["DEFAULT_FILETYPE"]["VID"]
+    mp4_ref_videos = test_convert_dir.glob(f"*{extension}")
 
-        mp4_test_video = test_data_tmp_dir / mp4_ref_video.name
+    for mp4_ref_video in mp4_ref_videos:
+        mp4_test_video = test_convert_tmp_dir / mp4_ref_video.name
 
         # Assert shapes of ref and test video arrays
 
@@ -74,7 +91,7 @@ def test_convert(test_data_tmp_dir: Path, test_data_dir: Path) -> None:
 
         # Assert size of ef and test video files
 
-        assert mp4_ref_video.stat().st_size == approx(
+        assert mp4_ref_video.stat().st_size == pytest.approx(
             mp4_test_video.stat().st_size, rel=0.01
         )
 
