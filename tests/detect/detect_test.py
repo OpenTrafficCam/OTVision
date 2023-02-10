017d0b58788cad5c9cca8e155ff520a0c0cfa887
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-02-10 17:21:33 +0100

    Test detect  returning correct otdet file

--- tests/detect/detect_test.py
+++ tests/detect/detect_test.py
@@ -1,6 +1,26 @@
+import shutil
+from filecmp import cmp
 from pathlib import Path
 
 import pytest
+import torch
+
+import OTVision.config as config
+from OTVision.detect.detect import main as detect
+from tests.conftest import YieldFixture
+
+
+@pytest.fixture
+def detect_test_data_dir(test_data_dir: Path) -> Path:
+    return test_data_dir / "detect"
+
+
+@pytest.fixture
+def detect_test_tmp_dir(test_data_tmp_dir: Path) -> YieldFixture[Path]:
+    detect_tmp_dir = test_data_tmp_dir / "detect"
+    detect_tmp_dir.mkdir()
+    yield detect_tmp_dir
+    shutil.rmtree(detect_tmp_dir)
 
 
 @pytest.fixture
@@ -22,3 +42,44 @@ def paths_with_legal_fileformats() -> list[Path]:
 @pytest.fixture
 def paths_with_illegal_fileformats() -> list[Path]:
     return [Path("err_a.video"), Path("err_b.image")]
+
+
+@pytest.fixture
+def cyclist_mp4(detect_test_data_dir: Path, detect_test_tmp_dir: Path) -> Path:
+    fname = "Testvideo_Cars-Cyclist_FR20_2020-01-01_00-00-00.mp4"
+    src = detect_test_data_dir / fname
+    dest = detect_test_tmp_dir / fname
+    shutil.copy2(src, dest)
+    return dest
+
+
+@pytest.fixture
+def truck_mp4(detect_test_data_dir: Path, detect_test_tmp_dir: Path) -> Path:
+    fname = "Testvideo_Cars-Truck_FR20_2020-01-01_00-00-00.mp4"
+    src = detect_test_data_dir / fname
+    dest = detect_test_tmp_dir / fname
+    shutil.copy2(src, dest)
+    return dest
+
+
+@pytest.fixture
+def default_cyclist_otdet(detect_test_data_dir: Path) -> Path:
+    fname = "Testvideo_Cars-Cyclist_FR20_2020-01-01_00-00-00.otdet"
+    return detect_test_data_dir / "default" / fname
+
+
+class TestDetect:
+    filetypes: list[str] = config.CONFIG[config.FILETYPES][config.VID]
+    model = torch.hub.load(
+        repo_or_dir="ultralytics/yolov5",
+        model="yolov5s",
+        pretrained=True,
+        force_reload=False,
+    )
+
+    def test_detect_mp4AsParam_returnCorrectOtdetFile(
+        self, detect_test_tmp_dir: Path, cyclist_mp4: Path, default_cyclist_otdet: Path
+    ) -> None:
+        detect([cyclist_mp4], force_reload_torch_hub_cache=True)
+        result_otdet = detect_test_tmp_dir / default_cyclist_otdet.name
+        assert cmp(result_otdet, default_cyclist_otdet)
