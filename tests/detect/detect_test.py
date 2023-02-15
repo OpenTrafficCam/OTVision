df399f4461e5d603e193fc8992b313d2fbd7ab09
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-02-15 18:11:05 +0100

    Add detect tests

--- tests/detect/detect_test.py
+++ tests/detect/detect_test.py
@@ -1,7 +1,9 @@
 import bz2
 import json
 import shutil
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Any
 
 import pytest
 import torch
@@ -10,6 +12,16 @@ from jsonschema import validate
 import OTVision.config as config
 from OTVision.detect.detect import main as detect
 from tests.conftest import YieldFixture
+
+METADATA = "metadata"
+DATA = "data"
+CLASSIFIED = "classified"
+CLASS = "class"
+CONF = "conf"
+X = "x"
+Y = "y"
+W = "w"
+H = "h"
 
 otdet_schema = {
     "type": "object",
@@ -68,41 +80,26 @@ otdet_schema = {
 }
 
 
-@pytest.fixture
+def read_bz2_otdet(otdet: Path) -> dict:
+    with bz2.open(otdet, "r") as file:
+        result_otdet_json = json.load(file)
+    return result_otdet_json
+
+
+@pytest.fixture(scope="module")
 def detect_test_data_dir(test_data_dir: Path) -> Path:
     return test_data_dir / "detect"
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def detect_test_tmp_dir(test_data_tmp_dir: Path) -> YieldFixture[Path]:
     detect_tmp_dir = test_data_tmp_dir / "detect"
-    detect_tmp_dir.mkdir()
+    detect_tmp_dir.mkdir(exist_ok=True)
     yield detect_tmp_dir
     shutil.rmtree(detect_tmp_dir)
 
 
-@pytest.fixture
-def paths_with_legal_fileformats() -> list[Path]:
-    return [
-        Path("vid_a.mov"),
-        Path("vid_b.mkv"),
-        Path("vid_c.avi"),
-        Path("vid_d.mpg"),
-        Path("vid_e.mpeg"),
-        Path("vid_f.m4v"),
-        Path("vid_g.wmv"),
-        Path("img_h.jpeg"),
-        Path("img_i.jpg"),
-        Path("img_j.png"),
-    ]
-
-
-@pytest.fixture
-def paths_with_illegal_fileformats() -> list[Path]:
-    return [Path("err_a.video"), Path("err_b.image")]
-
-
-@pytest.fixture
+@pytest.fixture(scope="module")
 def cyclist_mp4(detect_test_data_dir: Path, detect_test_tmp_dir: Path) -> Path:
     fname = "Testvideo_Cars-Cyclist_FR20_2020-01-01_00-00-00.mp4"
     src = detect_test_data_dir / fname
@@ -111,7 +108,7 @@ def cyclist_mp4(detect_test_data_dir: Path, detect_test_tmp_dir: Path) -> Path:
     return dest
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def truck_mp4(detect_test_data_dir: Path, detect_test_tmp_dir: Path) -> Path:
     fname = "Testvideo_Cars-Truck_FR20_2020-01-01_00-00-00.mp4"
     src = detect_test_data_dir / fname
@@ -120,41 +117,162 @@ def truck_mp4(detect_test_data_dir: Path, detect_test_tmp_dir: Path) -> Path:
     return dest
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def default_cyclist_otdet(detect_test_data_dir: Path) -> Path:
     fname = "Testvideo_Cars-Cyclist_FR20_2020-01-01_00-00-00.otdet"
     return detect_test_data_dir / "default" / fname
 
 
-class TestDetect:
-    filetypes: list[str] = config.CONFIG[config.FILETYPES][config.VID]
+@pytest.fixture(scope="module")
+def yolov5s() -> Any:
     model = torch.hub.load(
         repo_or_dir="ultralytics/yolov5",
         model="yolov5s",
         pretrained=True,
         force_reload=False,
-    ).cpu()
+    )
+    return model
 
-    def test_detect_otdet_valid_json(
-        self, detect_test_tmp_dir: Path, cyclist_mp4: Path
-    ) -> None:
-        detect([cyclist_mp4], model=self.model, force_reload_torch_hub_cache=False)
-        result_otdet = detect_test_tmp_dir / f"{cyclist_mp4.stem}.otdet"
-        assert result_otdet.exists()
+
+@dataclass
+class BoundingBox:
+    det_class: str
+    conf: float
+    x: float
+    y: float
+    w: float
+    h: float
+
+    @staticmethod
+    def from_dict(d: dict) -> "BoundingBox":
+        return BoundingBox(d[CLASS], d[CONF], d[X], d[Y], d[W], d[H])
+
+
+@dataclass
+class Detection:
+    bboxes: list[BoundingBox]
+
+    @staticmethod
+    def from_dict(d: dict) -> "Detection":
+        bboxes = [BoundingBox.from_dict(bbox) for bbox in d[CLASSIFIED]]
+        return Detection(bboxes)
+
+
+class TestDetect:
+    model: str = "yolov5s"
+    conf: float = 0.25
+    filetypes: list[str] = config.CONFIG[config.FILETYPES][config.VID]
+
+    @pytest.fixture(scope="class")
+    def result_cyclist_otdet(
+        self, cyclist_mp4: Path, detect_test_tmp_dir: Path
+    ) -> Path:
+        detect([cyclist_mp4], weights=self.model, force_reload_torch_hub_cache=False)
+
+        return detect_test_tmp_dir / f"{cyclist_mp4.stem}.otdet"
+
+    def is_normalized(self, bbox: BoundingBox) -> bool:
+        return (
+            (bbox.w >= 0 and bbox.w < 1)
+            and (bbox.y >= 0 and bbox.y < 1)
+            and (bbox.w >= 0 and bbox.w < 1)
+            and (bbox.h >= 0 and bbox.h < 1)
+        )
+
+    def test_detect_create_otdet(self, result_cyclist_otdet: Path) -> None:
+        assert result_cyclist_otdet.exists()
+
+    def test_detect_otdet_valid_json(self, result_cyclist_otdet: Path) -> None:
         try:
-            otdet_file = bz2.open(str(result_otdet), "r")
+            otdet_file = bz2.open(str(result_cyclist_otdet), "r")
             json.load(otdet_file)
         finally:
             otdet_file.close()
 
-    def test_detect_otdet_matches_schema(
-        self, detect_test_tmp_dir: Path, cyclist_mp4: Path, default_cyclist_otdet: Path
+    def test_detect_otdet_matches_schema(self, result_cyclist_otdet: Path) -> None:
+        assert result_cyclist_otdet.exists()
+
+        result_cyclist_otdet_json = read_bz2_otdet(result_cyclist_otdet)
+        assert result_cyclist_otdet
+        validate(result_cyclist_otdet_json, otdet_schema)
+
+    def test_detect_metadata_matches(
+        self, result_cyclist_otdet: Path, default_cyclist_otdet: Path
     ) -> None:
-        detect([cyclist_mp4], model=self.model, force_reload_torch_hub_cache=True)
-        result_otdet = detect_test_tmp_dir / default_cyclist_otdet.name
-        try:
-            file = bz2.open(str(result_otdet), "r")
-            result_otdet_json = json.load(file)
-            validate(result_otdet_json, otdet_schema)
-        finally:
-            file.close()
+        result_cyclist_metadata = read_bz2_otdet(result_cyclist_otdet)[METADATA]
+        expected_cyclist_metadata = read_bz2_otdet(default_cyclist_otdet)[METADATA]
+        assert result_cyclist_metadata == expected_cyclist_metadata
+
+    def test_detect_no_error_raised_on_wrong_filetype(
+        self, detect_test_tmp_dir: Path
+    ) -> None:
+        mkv_video_path = detect_test_tmp_dir / "video.vid"
+        mkv_video_path.touch()
+        detect(
+            paths=[mkv_video_path],
+            weights=self.model,
+            force_reload_torch_hub_cache=False,
+        )
+
+    def test_detect_bboxes_normalized(self, yolov5s: Any, truck_mp4: Path) -> None:
+        otdet_file = truck_mp4.parent / truck_mp4.with_suffix(".otdet")
+        otdet_file.unlink(missing_ok=True)
+        detect([truck_mp4], model=yolov5s, conf=0.25, normalized=True)
+        otdet_dict = read_bz2_otdet(otdet_file)
+
+        detections = [Detection.from_dict(det) for det in otdet_dict[DATA].values()]
+        for det in detections:
+            for bbox in det.bboxes:
+                assert self.is_normalized(bbox)
+                assert bbox.conf >= self.conf
+        otdet_file.unlink()
+
+    def test_detect_bboxes_denormalized(self, yolov5s: Any, truck_mp4: Path) -> None:
+        otdet_file = truck_mp4.parent / truck_mp4.with_suffix(".otdet")
+        otdet_file.unlink(missing_ok=True)
+        detect([truck_mp4], model=yolov5s, conf=0.25, normalized=False)
+        otdet_dict = read_bz2_otdet(otdet_file)
+
+        detections = [Detection.from_dict(det) for det in otdet_dict[DATA].values()]
+        denormalized_bbox_found = False
+        for det in detections:
+            for bbox in det.bboxes:
+                denormalized_bbox_found = (
+                    denormalized_bbox_found or not self.is_normalized(bbox)
+                )
+                assert bbox.conf >= self.conf
+        assert denormalized_bbox_found
+        otdet_file.unlink()
+
+    @pytest.mark.parametrize("conf", [0.0, 0.1, 0.5, 0.9, 1.0])
+    def test_detect_conf_bbox_above_thresh(
+        self, yolov5s: Any, truck_mp4: Path, conf: float
+    ) -> None:
+        otdet_file = truck_mp4.parent / truck_mp4.with_suffix(".otdet")
+        otdet_file.unlink(missing_ok=True)
+        detect(paths=[truck_mp4], model=yolov5s, conf=conf)
+        otdet_dict = read_bz2_otdet(otdet_file)
+
+        detections = [Detection.from_dict(det) for det in otdet_dict[DATA].values()]
+        for det in detections:
+            for bbox in det.bboxes:
+                assert bbox.conf >= conf
+        otdet_file.unlink()
+
+    @pytest.mark.parametrize("overwrite", [(True), (False)])
+    def test_detect_overwrite(
+        self, yolov5s: Any, truck_mp4: Path, overwrite: bool
+    ) -> None:
+        otdet_file = truck_mp4.parent / truck_mp4.with_suffix(".otdet")
+        otdet_file.unlink(missing_ok=True)
+        detect(paths=[truck_mp4], model=yolov5s, overwrite=True)
+
+        first_mtime = otdet_file.stat().st_mtime_ns
+        detect(paths=[truck_mp4], model=yolov5s, overwrite=overwrite)
+        second_mtime = otdet_file.stat().st_mtime_ns
+
+        if overwrite:
+            assert first_mtime != second_mtime
+        else:
+            assert first_mtime == second_mtime
+        otdet_file.unlink()
