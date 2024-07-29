879b1d52b57d4647712334e5bc436b303f006d4e
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2024-07-29 13:50:39 +0200

    Add workaround to use setuptools==71.1.0
    moviepy does not work with setuptools>=72 since it still depends
    on the deprecated TestCommand class which has been removed in
    setuptools>=72. Until the maintainers have fix this issue
    use setuptools==71.1.0.

--- install.sh
+++ install.sh
@@ -10,4 +10,6 @@ PIP="$VENV"/bin/pip
 python3.10 -m venv "$VENV"
 
 $PYTHON -m pip install --upgrade pip
+$PIP install --upgrade pip-tools pip wheel
+#$PIP install setuptools==71.1.0
 $PIP install -r requirements.txt --no-cache-dir
