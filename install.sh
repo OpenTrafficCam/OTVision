9452f39b776db34776dba78ef27537e9bbfd45dd
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2024-07-29 14:19:07 +0200

    Remove comment

--- install.sh
+++ install.sh
@@ -11,5 +11,4 @@ python3.10 -m venv "$VENV"
 
 $PYTHON -m pip install --upgrade pip
 $PIP install --upgrade pip-tools pip wheel
-#$PIP install setuptools==71.1.0
 $PIP install -r requirements.txt --no-cache-dir
