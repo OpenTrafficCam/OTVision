b2ab39e276312a589d42add6d7b29bc958602483
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2024-08-30 15:34:24 +0200

    Omit unsupported value -1 for defining batch size in yolo detect

--- OTVision/detect/yolo.py
+++ OTVision/detect/yolo.py
@@ -1,6 +1,7 @@
 """
 OTVision module to detect objects using yolov5
 """
+
 # Copyright (C) 2022 OpenTrafficCam Contributors
 # <https://github.com/OpenTrafficCam
 # <team@opentrafficcam.org>
@@ -153,7 +154,6 @@ class Yolov8(ObjectDetection):
             device=0 if torch.cuda.is_available() else "cpu",
             stream=True,
             verbose=False,
-            batch=-1,
             agnostic_nms=True,
         )
 
