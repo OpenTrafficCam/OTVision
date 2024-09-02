66a1f6931d7b918f0d3398c9eddd7ca8060006c6
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2024-09-02 16:49:28 +0200

    Fix install script for Linux and macOS not working when building application with respective release assets
    Dynamically determine the parent folder where install.sh is being executed.
    Otherwise, building OTVision with install.cmd on macOS will set the working directory
    to $HOME, which is the wrong directory to start the script.

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
 set -e
 echo "Install OTVision."
 
+echo "$DIR"
+cd "$DIR" || exit
 WORKING_DIR=$(pwd)
 VENV="$WORKING_DIR"/venv
 PYTHON="$VENV"/bin/python
 PIP="$VENV"/bin/pip
 
-python3.11 -m venv "$VENV"
+python3.10 -m venv "$VENV"
 
 $PYTHON -m pip install --upgrade pip
-$PIP install --upgrade pip-tools pip wheel
 $PIP install -r requirements.txt --no-cache-dir
