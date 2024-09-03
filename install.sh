e2357703e6166150b430c83109e0f11ba1da1b5a
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2024-09-03 15:06:19 +0200

    update install script to python 3.11

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ VENV="$WORKING_DIR"/venv
 PYTHON="$VENV"/bin/python
 PIP="$VENV"/bin/pip
 
-python3.10 -m venv "$VENV"
+python3.11 -m venv "$VENV"
 
 $PYTHON -m pip install --upgrade pip
 $PIP install -r requirements.txt --no-cache-dir
