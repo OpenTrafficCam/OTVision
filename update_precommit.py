19acc2f12450708a08470ad25324a71de40fb335
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2024-10-17 12:16:24 +0200

    Take into account extra-index-urls that are specified in requirements files for update precommit script

--- update_precommit.py
+++ update_precommit.py
@@ -3,6 +3,7 @@
 import re
 from abc import ABC, abstractmethod
 from copy import deepcopy
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Iterable
 
@@ -15,11 +16,27 @@ MYPY_REPOSITORY = "https://github.com/pre-commit/mirrors-mypy"
 HOOKS = "hooks"
 ADDITIONAL_DEPENDENCIES = "additional_dependencies"
 
+CAPTURE_GROUP_URL = "url"
 CAPTURE_GROUP_PACKAGE = "package"
 CAPTURE_GROUP_VERSION = "version"
 
 
-class Package(ABC):
+class AdditionalMypyDependency(ABC):
+
+    @abstractmethod
+    def __hash__(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __eq__(self, other: object) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def serialize(self) -> str:
+        raise NotImplementedError
+
+
+class Package(AdditionalMypyDependency):
     @property
     @abstractmethod
     def name(self) -> str:
@@ -28,10 +45,6 @@ class Package(ABC):
     @property
     @abstractmethod
     def version(self) -> str | None:
-        raise NotImplementedError
-
-    @abstractmethod
-    def __str__(self) -> str:
         raise NotImplementedError
 
     def __hash__(self) -> int:
@@ -50,13 +63,13 @@ class TypeStubPackage(Package):
 
     @property
     def version(self) -> str | None:
-        return None
+        return self._version
 
     def __init__(self, name: str, version: str | None) -> None:
         self._name = name
         self._version = version
 
-    def __str__(self) -> str:
+    def serialize(self) -> str:
         return self.name
 
 
@@ -73,10 +86,26 @@ class NormalPackage(Package):
         self._name = name
         self._version = version
 
-    def __str__(self) -> str:
+    def serialize(self) -> str:
         if self.version:
             return self.name + "==" + self.version
         return self.name
+
+
+@dataclass
+class ExtraIndexUrl(AdditionalMypyDependency):
+    url: str
+
+    def __hash__(self) -> int:
+        return hash(self.url)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ExtraIndexUrl):
+            return False
+        return self.url == other.url
+
+    def serialize(self) -> str:
+        return f"--extra-index-url={self.url}"
 
 
 class CustomDumper(yaml.SafeDumper):
@@ -84,14 +113,16 @@ class CustomDumper(yaml.SafeDumper):
         return super(CustomDumper, self).increase_indent(flow, False)
 
 
-def parse_multiple_requirements_file(files: Iterable[Path]) -> set[Package]:
-    packages: set[Package] = set()
+def parse_multiple_requirements_file(
+    files: Iterable[Path],
+) -> set[AdditionalMypyDependency]:
+    packages = set()
     for _file in files:
         packages.update(parse_requirements_file(_file))
     return packages
 
 
-def parse_requirements_file(requirements_file: Path) -> set[Package]:
+def parse_requirements_file(requirements_file: Path) -> set[AdditionalMypyDependency]:
     """Parse requirements.txt and extract package names using regex."""
     with open(requirements_file, "r") as file:
         lines = file.readlines()
@@ -109,17 +140,24 @@ def parse_requirements_file(requirements_file: Path) -> set[Package]:
     return packages
 
 
-def parse_requirement(requirement_line: str) -> Package | None:
+pattern_package = re.compile(
+    r"^(?!--extra-index-url)(?P<package>[a-zA-Z0-9_\-\.]+)(?:[<>=~!]+(?P<version>\S*))?"
+)
+pattern_extra_index_url = re.compile(r"^--extra-index-url\s+(?P<url>\S+)")
+
+
+def parse_requirement(requirement_line: str) -> AdditionalMypyDependency | None:
     """Extract package name from a requirement line using regex."""
     # Regex pattern to capture the package name, ignoring version specifiers
-    # pattern = re.compile(r"^([a-zA-Z0-9_\-\.]+)(?:[<>=~!]+\S*)?")
-    pattern = re.compile(
-        r"^(?P<package>[a-zA-Z0-9_\-\.]+)(?:[<>=~!]+(?P<version>\S*))?"
-    )
+    if match_extra_index_url := pattern_extra_index_url.match(requirement_line):
+        return create_extra_index_url(
+            match_extra_index_url.group(CAPTURE_GROUP_URL).strip()
+        )
 
-    match = pattern.match(requirement_line)
+    match = pattern_package.match(requirement_line)
     if not match:
         return None
+
     package_name = match.group(CAPTURE_GROUP_PACKAGE).strip()
     if package_version := match.group(CAPTURE_GROUP_VERSION):
         package_version = package_version.strip()
@@ -127,7 +165,11 @@ def parse_requirement(requirement_line: str) -> Package | None:
     return create_package(name=package_name, version=package_version)
 
 
-def create_package(name: str, version: str | None) -> Package:
+def create_extra_index_url(url: str) -> AdditionalMypyDependency:
+    return ExtraIndexUrl(url)
+
+
+def create_package(name: str, version: str | None) -> AdditionalMypyDependency:
     """Check if a type stub exists for a given package name and return it."""
     types_package_name = f"types-{name}"
     if __check_types_for_package_exists(types_package_name):
@@ -143,17 +185,19 @@ def __check_types_for_package_exists(package_name: str) -> bool:
     return response.status_code == 200
 
 
-def create_type_stub_package(name: str, version: str | None) -> Package:
+def create_type_stub_package(
+    name: str, version: str | None
+) -> AdditionalMypyDependency:
     return TypeStubPackage(name=name, version=version)
 
 
-def create_normal_package(name: str, version: str | None) -> Package:
+def create_normal_package(name: str, version: str | None) -> AdditionalMypyDependency:
     return NormalPackage(name=name, version=version)
 
 
-def serialize_packages(packages: Iterable[Package]) -> list[str]:
+def serialize_packages(packages: Iterable[AdditionalMypyDependency]) -> list[str]:
     """Converts packages to a serializable format."""
-    return sorted([str(package) for package in packages])
+    return sorted([package.serialize() for package in packages])
 
 
 def read_precommit_file(precommit_file: Path) -> dict:
@@ -201,13 +245,13 @@ def main() -> None:
     requirements_dev_file = Path("requirements-dev.txt")
     precommit_file = Path(".pre-commit-config.yaml")
 
-    packages = parse_multiple_requirements_file(
+    additional_dependencies = parse_multiple_requirements_file(
         [requirements_file, requirements_dev_file]
     )
-    serializable_packages = serialize_packages(packages)
+    serializable_dependencies = serialize_packages(additional_dependencies)
     precommit_config = read_precommit_file(precommit_file)
     updated_precommit_config = update_precommit_config(
-        precommit_config, serializable_packages
+        precommit_config, serializable_dependencies
     )
     save_precommit_config(updated_precommit_config, precommit_file)
 
