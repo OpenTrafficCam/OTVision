60f0273f90da7c6bba59ffbf7c4b8f30dc1be93c
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2025-03-08 23:49:25 +0100

    Add VideoTimestamper to add occurrence to each frame
    This time stamper is based on the current Timestamper class
    and is set to replace it when the object detection has been
    refactored to receive streams of image frames.

---
+++ OTVision/detect/timestamper.py
@@ -0,0 +1,136 @@
+import re
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
+
+from OTVision.application.detect.timestamper import Timestamper
+from OTVision.application.frame_count_provider import FrameCountProvider
+from OTVision.dataformat import FRAME
+from OTVision.domain.frame import Frame, FrameKeys
+from OTVision.helpers.date import parse_date_string_to_utc_datime
+from OTVision.helpers.files import (
+    FILE_NAME_PATTERN,
+    InproperFormattedFilename,
+    START_DATE,
+)
+from OTVision.helpers.video import get_duration
+
+
+class VideoTimestamper(Timestamper):
+    """Add timestamps when the frame occurred in real time to each frame.
+
+
+    Args:
+        video_file (Path): Path to the video file being processed.
+        expected_duration (timedelta | None): expected duration of the video used to
+            calculate the number of actual frames per second.
+        frame_count_provider (FrameCountProvider): Provider for the total number of
+            frames in a video file.
+    """
+
+    def __init__(
+        self,
+        video_file: Path,
+        expected_duration: timedelta | None,
+        frame_count_provider: FrameCountProvider,
+    ) -> None:
+        self._video_file = video_file
+        self._expected_duration = expected_duration
+
+        self._number_of_frames = frame_count_provider.provide(self._video_file)
+        self._time_per_frame = self._get_time_per_frame()
+
+    def stamp(self, frame: dict) -> Frame:
+        """This method adds timestamps when the frame occurred in real time to each
+        frame.
+
+        Returns:
+            Frame: frame with occurrence.
+        """
+        start_time = parse_start_time_from(self._video_file)
+        frame_number = frame[FRAME]
+        # Frame numbers start from 1
+        occurrence = start_time + (frame_number - 1) * self._time_per_frame
+
+        return Frame(
+            data=frame[FrameKeys.data],
+            frame=frame[FrameKeys.frame],
+            source=frame[FrameKeys.source],
+            occurrence=occurrence,
+        )
+
+    def _get_time_per_frame(self) -> timedelta:
+        """Calculates the duration for each frame. This is done using the total
+        duration of the video and the number of frames.
+
+        Returns:
+            timedelta: duration per frame
+        """
+        if self._expected_duration:
+            duration = self._expected_duration
+        else:
+            duration = get_duration(self._video_file)
+
+        return duration / self._number_of_frames
+
+
+def parse_start_time_from(video_file: Path) -> datetime:
+    """Parse the given filename and retrieve the start date of the video.
+
+    Args:
+        video_file (Path): path to video file
+
+    Raises:
+        InproperFormattedFilename: if the filename is not formatted as expected, an
+        exception will be raised
+
+    Returns:
+        datetime: start date of the video
+    """
+    match = re.search(
+        FILE_NAME_PATTERN,
+        video_file.name,
+    )
+    if match:
+        start_date: str = match.group(START_DATE)
+        return parse_date_string_to_utc_datime(start_date, "%Y-%m-%d_%H-%M-%S").replace(
+            tzinfo=timezone.utc
+        )
+
+    raise InproperFormattedFilename(f"Could not parse {video_file.name}.")
+
+
+class TimestamperFactory:
+    """
+    Factory class for creating timestamper instances.
+
+    This class provides methods to create and configure instances of the
+    Timestamper class or its derivatives.
+    """
+
+    def __init__(self, frame_count_provider: FrameCountProvider) -> None:
+        self._frame_count_provider = frame_count_provider
+
+    def create_video_timestamper(
+        self, video_file: Path, expected_duration: timedelta | None
+    ) -> Timestamper:
+        """Creates a video timestamper to handle video timestamping operations.
+
+        This function initializes a Timestamper object for the given video file,
+        allowing timestamp metadata to be processed or manipulated based on
+        the provided duration and frame count.
+
+        Args:
+            video_file (Path): the video file of frames to be timestamped.
+            expected_duration (timedelta | None): specifies the expected duration of the
+                video. If None, the duration is not pre-defined.
+
+        Returns:
+            Timestamper: An instance of Timestamper tailored to the provided video
+                file and parameters.
+
+        """
+        return VideoTimestamper(
+            video_file=video_file,
+            expected_duration=expected_duration,
+            frame_count_provider=self._frame_count_provider,
+        )
---
+++ tests/application/detect/test_timestamper.py
@@ -0,0 +1,115 @@
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
+from unittest.mock import Mock, patch
+
+import pytest
+
+from OTVision.application.frame_count_provider import FrameCountProvider
+from OTVision.detect.timestamper import VideoTimestamper, parse_start_time_from
+from OTVision.domain.frame import Frame, FrameKeys
+
+SOURCE = "path/to/Test-Cars_FR20_2022-02-03_04-00-00.mp4"
+EXPECTED_DURATION = timedelta(seconds=3)
+ACTUAL_DURATION = timedelta(seconds=4)
+NUMBER_OF_FRAMES = 60
+START_TIME = parse_start_time_from(Path(SOURCE))
+
+
+class TestParseStartTimeFromFilename:
+    @pytest.mark.parametrize(
+        "file_name, start_date",
+        [
+            (
+                "prefix_FR20_2022-01-01_00-00-00.mp4",
+                datetime(2022, 1, 1, 0, 0, 0, tzinfo=timezone.utc),
+            ),
+            (
+                "Test-Cars_FR20_2022-02-03_04-05-06.mp4",
+                datetime(2022, 2, 3, 4, 5, 6, tzinfo=timezone.utc),
+            ),
+            (
+                "Test_Cars_FR20_2022-02-03_04-05-06.mp4",
+                datetime(2022, 2, 3, 4, 5, 6, tzinfo=timezone.utc),
+            ),
+            (
+                "Test_Cars_2022-02-03_04-05-06.mp4",
+                datetime(2022, 2, 3, 4, 5, 6, tzinfo=timezone.utc),
+            ),
+            (
+                "2022-02-03_04-05-06.mp4",
+                datetime(2022, 2, 3, 4, 5, 6, tzinfo=timezone.utc),
+            ),
+            (
+                "2022-02-03_04-05-06-suffix.mp4",
+                datetime(2022, 2, 3, 4, 5, 6, tzinfo=timezone.utc),
+            ),
+        ],
+    )
+    def test_get_start_time_from(self, file_name: str, start_date: datetime) -> None:
+        parsed_date = parse_start_time_from(Path(file_name))
+
+        assert parsed_date == start_date
+
+
+class TestVideoTimestamper:
+
+    @pytest.mark.parametrize("expected_duration", [EXPECTED_DURATION, None])
+    @patch("OTVision.detect.timestamper.get_duration", return_value=ACTUAL_DURATION)
+    def test_stamp_frame(
+        self, mock_get_duration: Mock, expected_duration: timedelta | None
+    ) -> None:
+        first_frame = create_frame_without_occurrence(1)
+        second_frame = create_frame_without_occurrence(2)
+        third_frame = create_frame_without_occurrence(3)
+        given_frame_count_provider = create_frame_count_provider()
+
+        if expected_duration is not None:
+            time_per_frame = EXPECTED_DURATION / NUMBER_OF_FRAMES
+        else:
+            time_per_frame = ACTUAL_DURATION / NUMBER_OF_FRAMES
+
+        target = VideoTimestamper(
+            video_file=Path(SOURCE),
+            expected_duration=expected_duration,
+            frame_count_provider=given_frame_count_provider,
+        )
+        actual_first_frame = target.stamp(first_frame)
+        actual_second_frame = target.stamp(second_frame)
+        actual_third_frame = target.stamp(third_frame)
+
+        assert actual_first_frame == create_expected_frame(first_frame, START_TIME)
+        assert actual_second_frame == create_expected_frame(
+            second_frame, START_TIME + time_per_frame
+        )
+        assert actual_third_frame == create_expected_frame(
+            third_frame, START_TIME + 2 * time_per_frame
+        )
+        given_frame_count_provider.provide.assert_called_once_with(Path(SOURCE))
+
+        if expected_duration is None:
+            mock_get_duration.assert_called_once_with(Path(SOURCE))
+        else:
+            mock_get_duration.assert_not_called()
+
+
+def create_frame_without_occurrence(frame_number: int) -> dict:
+    return {
+        FrameKeys.data: Mock(),
+        FrameKeys.frame: frame_number,
+        FrameKeys.source: SOURCE,
+    }
+
+
+def create_frame_count_provider() -> Mock:
+    mock = Mock(spec=FrameCountProvider)
+    mock.provide.return_value = NUMBER_OF_FRAMES
+    return mock
+
+
+def create_expected_frame(raw_data: dict, occurrence: datetime) -> Frame:
+    return Frame(
+        data=raw_data[FrameKeys.data],
+        frame=raw_data[FrameKeys.frame],
+        source=raw_data[FrameKeys.source],
+        occurrence=occurrence,
+    )
