e57c5a8ea158cca5207b262dc3086dde7d42e9bf
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2025-03-21 18:07:30 +0100

    Let get_files support files as string
    Helps to ease transition to different types of sources (i.e. RTSP stream, files).

--- tests/helpers/files_test.py
+++ tests/helpers/files_test.py
@@ -94,17 +94,16 @@ def test_get_files_dirPathAsListOfPathObjectAs1stParam_returnsCorrectList(
     assert xml_file_path in files
 
 
-def test_get_files_dirPathsAsListOfString_RaiseTypeError(
+def test_get_files_supports_strings(
     test_dir_with_files: Path,
 ) -> None:
-    list_of_str = [
-        str(Path(test_dir_with_files, "img_1.PNG")),
-        Path(test_dir_with_files, "img_2.png"),
-        Path(test_dir_with_files, "img_3.PnG"),
-    ]
+    first_image = Path(test_dir_with_files, "img_1.PNG")
+    second_image = Path(test_dir_with_files, "img_2.png")
+    third_image = Path(test_dir_with_files, "img_3.PnG")
+    list_of_str = [str(first_image), str(second_image), str(third_image)]
 
-    with pytest.raises(TypeError):
-        get_files(paths=list_of_str)  # type: ignore
+    files = get_files(paths=list_of_str)
+    assert set(files) == {first_image, second_image, third_image}
 
 
 def test_get_files_dirPathsAsParam_RaiseTypeError(test_dir_with_files: Path) -> None:
