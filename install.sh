a819509a0cc49746bc03e252f2d77dbacbec3cf5
Lars Briem <lars.briem@platomo.de>
2025-08-07 12:31:07 +0200

    Fix linter

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ WORKING_DIR=$(pwd)
 VENV="$WORKING_DIR"/.venv
 
 # Check if uv is available globally, if not install it
-if ! command -v uv &> /dev/null; then
+if ! command -v uv &>/dev/null; then
   echo "uv not found globally, installing uv..."
   curl -LsSf https://astral.sh/uv/install.sh | sh
   export PATH="$HOME/.cargo/bin:$PATH"
