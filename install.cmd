1c32dfb504023878f955d1b0e2421ffbfd6409f6
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2025-08-11 15:30:53 +0200

    Fix uv not being installed on Windows using the install script

--- install.cmd
+++ install.cmd
@@ -1,6 +1,6 @@
 echo Install OTVision.
 @echo off
-FOR /F "tokens=* USEBACKQ" %%F IN (`python --version`) DO SET PYTHON_VERSION=%%F
+FOR /F "tokens=* USEBACKQ" %%F IN (`python --version`) DO SET "PYTHON_VERSION=%%F"
 
 echo %PYTHON_VERSION%
 if "x%PYTHON_VERSION:3.12=%"=="x%PYTHON_VERSION%" (
@@ -9,10 +9,10 @@ if "x%PYTHON_VERSION:3.12=%"=="x%PYTHON_VERSION%" (
 
 REM Check if uv is available globally, if not install it
 where uv >nul 2>nul
-if %errorlevel% neq 0 (
+if errorlevel neq 0 (
     echo uv not found globally, installing uv...
-    powershell -Command "irm https://astral.sh/uv/install.ps1 | iex"
-    set PATH=%USERPROFILE%\.cargo\bin;%PATH%
+    powershell -ExecutionPolicy Bypass -Command "& {irm https://astral.sh/uv/install.ps1 | iex}"
+    set "PATH=%USERPROFILE%\.cargo\bin;%PATH%"
 )
 
 uv venv .venv
